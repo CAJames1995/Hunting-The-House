25ba599aedb93d085de616194bd7127851fcde7d
Aliyah <Aliyahjohnx@gmail.com>
2023-04-11 18:52:40 -0400

    dog appears but mesh glitches: not catchable

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -8,8 +8,9 @@ public class ARController : MonoBehaviour
     public GameObject Pubbles;
     public GameObject Almond;
     public ARRaycastManager RaycastManager;
-    //public ARPlaneManager plane;
-    //public ARPlane activeplane;
+    public ARPlaneManager plane;
+    public ARPlane activeplane;
+    public GameObject appearsound;
 
     private float nextActionTime = 0.0f;
     private int shortperiod = 10, longperiod = 35;
@@ -18,7 +19,6 @@ public class ARController : MonoBehaviour
     private bool act;
 
 
-    //OLD CODE USING RANDOM POSITION.NOW USING POSITION BASED ON PLANE DETECTED
     //get Random X within a range
     public float getRandomX()
     {
@@ -33,7 +33,7 @@ public class ARController : MonoBehaviour
     {
         float random;
 
-        random = Random.Range(100, 6000);
+        random = Random.Range(500, 900);
         return random;
     }
 
@@ -42,7 +42,7 @@ public class ARController : MonoBehaviour
     {
         float random;
 
-        random = Random.Range(-650, 1969);
+        random = Random.Range(-10, -20);
         return random;
     }
 
@@ -51,6 +51,7 @@ public class ARController : MonoBehaviour
     public float randY()
     {
         float random = Random.Range(-110, -189);
+        appearsound.SetActive(true);
         return random;
     }
 
@@ -59,9 +60,10 @@ public class ARController : MonoBehaviour
     private void Start()
     {
         count++;
+        appearsound.SetActive(false);
 
-        //plane = GetComponent<ARPlaneManager>();
-        //plane.planesChanged += PlaneChanged;
+        plane = GetComponent<ARPlaneManager>();
+        plane.planesChanged += PlaneChanged;
 
         //act = Timer.act;
         difficulty = DifficultyScript.levelint;//get the level value
@@ -86,29 +88,27 @@ public class ARController : MonoBehaviour
         }
     }
 
-    //private void PlaneChanged(ARPlanesChangedEventArgs args)
-    //{
-    //    randomRot = new Quaternion(20, randY(), 3, 0);
-
-    //    if (args.added != null)
-    //    {
-    //        activeplane = args.added[0]; //detect first plane
-            
-    //    }
-    //}
+    private void PlaneChanged(ARPlanesChangedEventArgs args)
+    {
+        if (args.added != null)
+        {
+           activeplane = args.added[0]; //save first plane detected
+        }
+    }
 
 
     // Update is called once per frame
     private void Update()
     {
-        //plane.planesChanged += PlaneChanged;
+        plane.planesChanged += PlaneChanged;
 
         //Random X, Z Spawn. Y remains 5 above plane
         Vector3 randomSpawn = new Vector3(getRandomX(), getRandomY(), getRandomZ());
         //Random Rotation
         Quaternion randomRot = new Quaternion(20, randY(), 3, 0);
 
-
+        Vector3 appendtoPlanePos = new Vector3(getRandomX(), -20, 500);
+        //ideal rand x, -20, 500
 
         if (count != limit)
         {
@@ -116,41 +116,39 @@ public class ARController : MonoBehaviour
             if (Time.time > nextActionTime)
             {
                 nextActionTime = Time.time + shortperiod;
-                GameObject.Instantiate(Almond, randomSpawn, randomRot);
-                //GameObject.Instantiate(Almond, activeplane.transform.position, randomRot);
+                //GameObject.Instantiate(Almond, randomSpawn, randomRot); // please see notes on trello card: Phone Tests
+                GameObject.Instantiate(Almond, appendtoPlanePos += activeplane.transform.position, randomRot);
+
                 count++;
             }
 
 
-            //Goal is to set to 15 Seconds
-            if (Time.time > nextActionTime)
-            {
-                nextActionTime = Time.time + longperiod;
-                GameObject.Instantiate(Pubbles, randomSpawn, randomRot);
-                //GameObject.Instantiate(Pubbles, activeplane.transform.position, randomRot);
-                count++;
-            }
+            ////Goal is to set to 15 Seconds
+            //if (Time.time > nextActionTime)
+            //{
+            //    nextActionTime = Time.time + longperiod;
+            //    //GameObject.Instantiate(Pubbles, randomSpawn, randomRot);
+            //    GameObject.Instantiate(Pubbles, activeplane.transform.position, randomRot);
+            //    count++;
+            //}
         }
 
+        ////OLD CONTROLS BASED ON TOUCH:
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
+        //    List<ARRaycastHit> touches = new List<ARRaycastHit>();
+        //    RaycastManager.Raycast(Input.GetTouch(0).position, touches, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
 
+        //    if (touches.Count % 2 == 0)
+        //    {
+        //        GameObject.Instantiate(Pubbles, touches[0].pose.position, touches[0].pose.rotation);
+        //    }
+        //    else
+        //    {
+        //        GameObject.Instantiate(Almond, touches[0].pose.position, touches[0].pose.rotation);
+        //    }
+        //}
 
-
-        //OLD CONTROLS BASED ON TOUCH:
-
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            List<ARRaycastHit> touches = new List<ARRaycastHit>();
-            RaycastManager.Raycast(Input.GetTouch(0).position, touches, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-
-            if (touches.Count % 2 == 0)
-            {
-                GameObject.Instantiate(Pubbles, touches[0].pose.position, touches[0].pose.rotation);
-            }
-            else
-            {
-                GameObject.Instantiate(Almond, touches[0].pose.position, touches[0].pose.rotation);
-            }
-        }
 
 
     }
